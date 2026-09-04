aab658fdded1906a19dbe8884f346c2e37e58beb
=basmala-mustafa <=basmala.mustafa@amit-learning.com>
2026-09-04 19:22:38 +0300

    add tags of file index.html & script.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-98--maadi
 Frist Repository Project
+
+edit file index.html  & script.js
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const input_text = document.getElementById("text-input");
+// const input_text = document.getElementById("text-input");
 
+console.log("Hello Git Hub");
 
 
 // console.log(input_text.value);
